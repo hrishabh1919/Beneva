3347e6e59ae71b71b8523de90ba978ecd6d57c43
hrishabh1919 <hrishiofficial01@gmail.com>
2025-05-02 02:59:39 +0530

    runnable code

---
+++ app/src/main/java/com/example/beneva/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.beneva
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_activity)
+
+        // Delay for 2 seconds and then navigate to LoginActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
